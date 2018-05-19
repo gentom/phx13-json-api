2fb6b80fc0edf8a43cdabe2d2ffea76e57020d49
gentom <gento.m.developer@gmail.com>
2018-05-19 20:46:17 +0900

    add hash_user_password function

--- lib/api13/auth/user.ex
+++ lib/api13/auth/user.ex
@@ -17,5 +17,15 @@ defmodule Api13.Auth.User do
     |> cast(attrs, [:email, :password, :is_active])
     |> validate_required([:email, :password, :is_active])
     |> unique_constraint(:email)
+    |> hash_user_password()
   end
+
+  defp hash_user_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp hash_user_password(changeset) do
+    changeset
+  end
+
 end
