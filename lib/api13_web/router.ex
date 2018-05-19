ea84c3f5e502d1c8f360718257f611a17b1d165a
gentom <gento.m.developer@gmail.com>
2018-05-19 21:52:36 +0900

    protect a resource with authentication

--- lib/api13_web/router.ex
+++ lib/api13_web/router.ex
@@ -6,9 +6,31 @@ defmodule Api13Web.Router do
     plug :fetch_session
   end
 
+  pipeline :api_auth do
+    plug :ensure_authenticated
+  end
+
   scope "/api", Api13Web do
     pipe_through :api
-    resources "/users", UserController, except: [:new, :edit]
     post "/users/sign_in", UserController, :sign_in
   end
+
+  scope "/api", Api13Web do
+    pipe_through [:api, :api_auth]
+    resources "/users", UserController, except: [:new, :edit]
+  end
+
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render(Api13Web.ErrorView, "401.json", message: "Unauthenticated user")
+      |> halt()
+    end
+  end
+
 end
