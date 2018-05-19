970a55dec430d318254e015af8085f4a4addcf1f
gentom <gento.m.developer@gmail.com>
2018-05-19 21:11:42 +0900

    add plug Corsica

--- lib/api13_web/endpoint.ex
+++ lib/api13_web/endpoint.ex
@@ -35,6 +35,11 @@ defmodule Api13Web.Endpoint do
     key: "_api13_key",
     signing_salt: "+hhWukyd"
 
+  plug Corsica, origins: "http://localhost:8080",
+    log: [rejected: :error, invalid: :warn, accepted: :debug],
+    allow_headers: ["content-type"],
+    allow_credentials: true
+
   plug Api13Web.Router
 
   @doc """
