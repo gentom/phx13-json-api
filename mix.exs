b8acba51992eee2c67c4c8a871b06b750ddac5f5
gentom <gento.m.developer@gmail.com>
2018-05-19 21:09:12 +0900

    add corsica for CORS

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Api13.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:bcrypt_elixir, "~> 1.0"}
+      {:bcrypt_elixir, "~> 1.0"},
+      {:corsica, "~> 1.0"}
     ]
   end
 
