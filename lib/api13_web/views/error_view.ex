b754cd07bd01c9414b609ba260b27827ad02f063
gentom <gento.m.developer@gmail.com>
2018-05-20 18:39:21 +0900

    add render 404s & 500s json responses functionality

--- lib/api13_web/views/error_view.ex
+++ lib/api13_web/views/error_view.ex
@@ -13,4 +13,12 @@ defmodule Api13Web.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Endpoint not found!"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal server error :("}}
+  end
 end
